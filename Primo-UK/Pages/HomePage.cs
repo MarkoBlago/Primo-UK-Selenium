56a9ddcc094175d9920b8e7e2ec209cf0a21e634
Marko <berkomare@gmail.com>
2021-11-20 21:26:56 +0100

    Test Commit

--- Primo-UK/Pages/HomePage.cs
+++ Primo-UK/Pages/HomePage.cs
@@ -15,15 +15,66 @@ namespace Primo_UK.Pages
             this.driver = driver;
         }
 
+
+        #region Elements
+
+        public IWebElement CustomerIcon()
+        {
+            return driver.FindElement(By.XPath("//header/div[2]/div[1]/ul[1]/li[1]/a[1]"));
+        }
+
+        private IWebElement EmailField()
+        {
+            return driver.FindElement(By.Id("ajaxlogin-email"));
+        }
+
+        private IWebElement PasswordField()
+        {
+            return driver.FindElement(By.Id("ajaxlogin-pass"));
+        }
+
+        private IWebElement SubmitButton()
+        {
+            return driver.FindElement(By.Id("ajaxlogin-send"));
+        }
+
+        #endregion
+
+        #region Actions
+
+
+        public void CustomerIcon_Click()
+        {
+            CustomerIcon().Click();
+        }
+
+        public void EmailField_Fill()
+        {
+            EmailField().SendKeys("mareberko@gmail.com");
+        }
+
+
+        public void PasswordField_Fill()
+        {
+            PasswordField().SendKeys("123qwe!@#QWE");
+        }
+
+
+        public void SubmitButton_Click()
+        {
+            SubmitButton().Click();
+        }
+
         public void GoToURL()
         {
             driver.Navigate().GoToUrl("https://water-coolers.ru/");
         }
 
-        public static void GoToURLMethod()
-        {
+        #endregion
 
-        }
+
+
+
 
     }
 }
--- Primo-UK/Tests/EndToEnd.cs
+++ Primo-UK/Tests/EndToEnd.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Primo_UK.Tests
@@ -12,12 +13,19 @@ namespace Primo_UK.Tests
     public class EndToEnd : Browser
     {
 
-
         [Test]
         public static void LoginAndGoThruCheckout()
         {
             HomePage HP = new HomePage(driver);
+
             HP.GoToURL();
+
+            HP.CustomerIcon_Click();
+            HP.EmailField_Fill();
+            HP.PasswordField_Fill();
+            HP.SubmitButton_Click();
+
+            Thread.Sleep(5000);
 
         }
 
